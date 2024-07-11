e1d8954bc4b0c80c882f98d2e1644ba199ba39da
ji252303 <qordlstn629@naver.com>
2024-07-12 02:38:52 +0900

    page SEO set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,13 @@ import Main from '../components/section/Main'
 
 const Home = () => {
     return (
-        <Main>
+        <Main 
+            title = "유튜브"
+            description="유튜버 사이트에 오신 것을 환영합니다.">
             Home
         </Main>
     )
 }
 
 export default Home
+
